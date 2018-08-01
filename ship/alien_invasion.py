dccf3020fbadd514e4450b2e42dd69639665b05f
肖 <38874018+XXXKEKE@users.noreply.github.com>
2018-08-01 21:39:41 +0800

    Add files via upload

---
+++ ship/alien_invasion.py
@@ -0,0 +1,69 @@
+import sys
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard 
+from button import Button
+from ship import Ship
+#from alien import Alien
+import game_functions as gf 
+
+def run_game():
+	#初始化pygame，设置和屏幕对象
+
+	pygame.init()
+	ai_settings=Settings()
+	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+
+	#创建piay按钮
+	play_button=Button(ai_settings,screen,"play")
+
+
+	#创建一个用于存储游戏统计信息的实例
+	stats=GameStats(ai_settings)
+	sb = Scoreboard(ai_settings,screen,stats)
+
+	#设置背景色
+	#bg_color = (230,230,230)
+
+	#创建一艘飞船
+	ship=Ship(ai_settings,screen)
+
+	#创建一个用于存储子弹的数组
+	bullets=Group()
+
+	#创建一个外星人编组
+	aliens=Group()
+	gf.creat_fleet(ai_settings,screen,ship,aliens)
+
+	#创建一个外星人 
+	#alien=Alien(ai_settings,screen)
+
+	# 开始游戏的主循环
+
+	while True:
+		#监视键盘合鼠标事件
+		'''
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				sys.exit()'''
+		gf.check_events(ai_settings, screen,stats,sb,play_button, ship,aliens, bullets)
+		if stats.game_active:
+			
+			ship.update() 
+			'''
+
+			# 每次循环时都重新绘制屏幕
+			screen.fill(ai_settings.bg_color)
+			ship.blitme()
+
+			# 让最近绘制的屏幕可见
+			pygame.display.flip()'''
+			gf.update_bullets(ai_settings,screen,stats,sb,ship,aliens,bullets)
+			gf.update_aliens(ai_settings,screen,stats,sb,ship,aliens,bullets)
+		gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
+
+run_game()
